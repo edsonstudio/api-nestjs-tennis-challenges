c1d1ec9b2473a7e9dbc131177c26184592fe20ad
edsonstudio <edsont8@hotmail.com>
2024-01-03 15:56:02 -0300

    Criado endpoint de testes para criar / atualizar um jogador, criada a dto responsavel pelos dados da criacao do jogador e criada a interface jogador

--- src/jogadores/controllers/jogadores.controller.ts
+++ src/jogadores/controllers/jogadores.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from '../dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
-    
+
     @Post()
-    async criarAtualizarJogador() {
+    async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+        const { email, nome } = criarJogadorDto;
         return JSON.stringify({
-            "nome": "Edson"
+            "nome": `${nome}`,
+            "email": `${email}`
         });
     }
 }
