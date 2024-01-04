54d1fb3fb441c63991c5ded0f0a0a670601cb112
edsonstudio <edsont8@hotmail.com>
2024-01-04 09:16:38 -0300

    Adicionado o pacote Mongoose para utlizar o banco de dados MongoDB, importado o modulo do Mongoose no app.module e aplicada a connection-string do banco na nuvem, criado o schema jogador referente ao modelo, ajustada a interface Jogador para extender da classe Document do mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net/tennischallenges'),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
