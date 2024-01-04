f5d0c8cce529c85227ae596b3a6045a0c6febfc0
edsonstudio <edsont8@hotmail.com>
2024-01-04 08:42:50 -0300

    Criados metodos no service de jogadores para consultar jogador por email e para deletar jogador por email, criado endpoint para deletar jogador e ajustado o endpoint de consultar todos os jogadores para que tambem consulte por email

--- src/jogadores/controllers/jogadores.controller.ts
+++ src/jogadores/controllers/jogadores.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from '../dtos/criar-jogador.dto';
 import { JogadoresService } from '../services/jogadores.service';
 import { Jogador } from '../interfaces/jogador.interface';
@@ -7,13 +7,22 @@ import { Jogador } from '../interfaces/jogador.interface';
 export class JogadoresController {
     constructor(private readonly jogadoresService: JogadoresService) { }
 
-    @Post()
+    @Post() // Ignorada a boa pratica de criar um endpoint para cada ação, apenas para testes.
     async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
         await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
     }
 
-    @Get()
-    async consultarJogadores(): Promise<Jogador[]> {
-        return this.jogadoresService.consultarTodosJogadores(); 
+    @Get() // Ignorada a boa pratica de criar um endpoint para cada ação, apenas para testes.
+    async consultarJogadores(@Query('email') email: string): Promise<Jogador[] | Jogador> {
+        if (email) {
+            return this.jogadoresService.consultarJogadorPorEmail(email);
+        } else {
+            return this.jogadoresService.consultarTodosJogadores();   
+        }
+    }
+
+    @Delete()
+    async deletarJogador(@Query('email') email: string): Promise<void> {
+        await this.jogadoresService.deletarJogador(email);
     }
 }
--- src/jogadores/services/jogadores.service.ts
+++ src/jogadores/services/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { v4 as uuid } from 'uuid';
 import { CriarJogadorDto } from "../dtos/criar-jogador.dto";
 import { Jogador } from "../interfaces/jogador.interface";
@@ -12,9 +12,19 @@ export class JogadoresService {
         return await this.jogadores;
     }
 
+    async consultarJogadorPorEmail(email: string): Promise<Jogador> {
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
+
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`O jogador com o email: ${email} não foi encontrado!`);
+        }
+
+        return jogadorEncontrado;
+    }
+
     async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
         const { email } = criarJogadorDto;
-        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email);
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
 
         if (jogadorEncontrado) {
             await this.atualizar(jogadorEncontrado, criarJogadorDto);
@@ -22,6 +32,15 @@ export class JogadoresService {
             await this.criar(criarJogadorDto);
         }
 
+    }
+
+    async deletarJogador(email: string): Promise<void> {
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`O jogador com o email: ${email} não foi encontrado!`);
+        }
+
+        this.jogadores = this.jogadores.filter(jogador => jogador.email !== jogadorEncontrado.email);
     }
 
     private async criar(criarJogadorDto: CriarJogadorDto): Promise<void> {
