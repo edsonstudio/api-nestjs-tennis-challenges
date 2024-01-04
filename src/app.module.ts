c5d023fab7cded4c6430ed5498535acacdaba471
edsonstudio <edsont8@hotmail.com>
2024-01-04 14:22:23 -0300

    Criado o diretorio modules para organizar todos os modulos, movidos todos os arquivos do modulo jogadores para o diretorio modules, criado o novo modulo Categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { JogadoresModule } from './jogadores/jogadores.module';
+import { JogadoresModule } from './modules/jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './modules/categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net/tennischallenges'),
-    JogadoresModule
+    CategoriasModule,
+    JogadoresModule,
   ],
   controllers: [],
   providers: [],
