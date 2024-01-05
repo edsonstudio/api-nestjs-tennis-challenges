d9a8eb19afb970d41497a556ca33f76cbd8157f1
edsonstudio <edsont8@hotmail.com>
2024-01-05 08:41:27 -0300

    Criados os modulos Partidas e Desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+
+import { CategoriasModule } from './modules/categorias/categorias.module';
+import { DesafiosModule } from './modules/desafios/desafios.module';
 import { JogadoresModule } from './modules/jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CategoriasModule } from './modules/categorias/categorias.module';
+import { PartidasModule } from './modules/partidas/partidas.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net/tennischallenges'),
     CategoriasModule,
+    DesafiosModule,
     JogadoresModule,
+    MongooseModule.forRoot('mongodb+srv://edsonstudio:rud6JHpd8oqJJukV@cluster0.d8g8xgw.mongodb.net/tennischallenges'),
+    PartidasModule,
   ],
   controllers: [],
   providers: [],
